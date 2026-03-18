5c572e46341cabf2cd656124edd7cd27d36574ed
Devansh Kaushik <devanshisdevansh2007@gmail.com>
2026-03-19 01:11:00 +0530

    Add routes for ForgotPassword and ResetPassword pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import { Switch, Route } from "wouter";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -20,7 +22,8 @@ function Router() {
       <Route path="/" component={Landing} />
       <Route path="/login" component={() => <Auth isLogin={true} />} />
       <Route path="/signup" component={() => <Auth isLogin={false} />} />
-      
+      <Route path="/forgot-password" component={ForgotPassword} />
+<Route path="/reset-password/:token" component={ResetPassword} />
       {/* Protected Routes */}
       <Route path="/dashboard" component={() => <ProtectedRoute component={Dashboard} />} />
       <Route path="/marketplace" component={() => <ProtectedRoute component={Marketplace} />} />
